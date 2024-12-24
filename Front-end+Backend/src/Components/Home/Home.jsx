5eb9353281ccf96d3d3f903cb04e6881ec57b421
MAbdullah369 <bscs22111@itu.edu.pk>
2024-12-24 13:05:01 +0500

    Home-Page

--- Front-end+Backend/src/Components/Home/Home.jsx
+++ Front-end+Backend/src/Components/Home/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar.jsx';
+import { useEffect } from 'react';
+import './Home.css'
 
 const Home = () => {
   return (
-    <div><h1>Home</h1></div>
+    
+    <div>
+        <Navbar/>
+        <img src="https://dwtr67e3ikfml.cloudfront.net/bookCovers/85d776b5-7c55-4e8e-bffa-d6b2567efb20.jpg__300x0"  alt=""  height= {350} />
+        <img src="https://dwtr67e3ikfml.cloudfront.net/bookCovers/88aad336-6bfe-451b-8065-f53238d8b7aa.jpg__300x0" alt="" height= {350}/>
+<img src="https://dwtr67e3ikfml.cloudfront.net/bookCovers/56b13e1e-8f22-4ce8-a61d-f5e0ab20c960.jpg__300x0" alt="" height= {350}/>
+    </div>
   )
 }
 
---
+++ Front-end+Backend/src/Components/Home/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+        <h2>Book Tracking</h2>
+        <a href="">About</a>
+        <a href="">Genres</a>
+        <a href="">Register</a>
+        </div> 
+    </div>
+  )
+}
+
+export default Navbar
